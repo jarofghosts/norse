b969e8f70e7228f17a64d044ce8c6e1c7285d26d
jesse keane <me@jessekeane.me>
2014-04-12 18:18:43 -0700

    cleanup

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var through = require('through')
 module.exports = norse
 
 function norse(_time_unit) {
-  var time_unit = +(_time_unit || 500),
-      norse_stream = through(to_timing)
+  var time_unit = +(_time_unit || 500)
 
-  var DASH = time_unit * 3,
-      DOT = time_unit,
-      INTER_DOTDASH = time_unit,
-      INTER_LETTER = time_unit * 3,
-      INTER_WORD = time_unit * 7
+  var norse_stream = through(to_timing)
+
+  var DASH = time_unit * 3
+    , DOT = time_unit
+
+  var INTER_DOTDASH = time_unit
+    , INTER_LETTER = time_unit * 3
+    , INTER_WORD = time_unit * 7
 
   var conversion = {
     '.': DOT,
@@ -21,21 +23,22 @@ function norse(_time_unit) {
   return norse_stream
 
   function to_timing(data) {
-    var word = data.toString(),
-        letters = word.split(' '),
-        buf,
-        bit
+    var word = data.toString()
 
-    for (var i = 0, l = letters.length; i < l; ++i) {
-      if (i) norse_stream.queue([buf, INTER_LETTER])
+    var letters = word.split(' ')
+      , buf
+      , bit
+
+    for(var i = 0, l = letters.length; i < l; ++i) {
+      if(i) norse_stream.queue([buf, INTER_LETTER])
       translate_letters(letters[i])
     }
 
     norse_stream.queue([buf, INTER_WORD])
 
     function translate_letters(letter) {
-      var bits = letter.split(''),
-          bit
+      var bits = letter.split('')
+        , bit
 
       for (var i = 0, l = bits.length; i < l; ++i) {
         if (i) norse_stream.queue([buf, INTER_DOTDASH])
